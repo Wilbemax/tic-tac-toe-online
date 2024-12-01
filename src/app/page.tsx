c684ddf4560f488459b6fd6e1cd4919fc0871bf5
Willbemax <max22042007@yandex.ru>
2024-12-01 15:39:15 +0300

    feet: prettier of all project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 
 export default async function Home() {
-
-  const games = await prisma.game.findMany()
+  const games = await prisma.game.findMany();
   console.log(games);
 
   return (
     <div>
-      <Button size={'lg'} variant={"outline"}> GG</Button>
+      <Button size={"lg"} variant={"outline"}>
+        {" "}
+        GG
+      </Button>
 
       {games.map((game) => (
         <p key={game.id}>{game.name}</p>
