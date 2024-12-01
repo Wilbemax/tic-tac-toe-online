39ec8972a4087d18eb306e03458f62ea7cbde555
Willbemax <max22042007@yandex.ru>
2024-12-01 15:29:24 +0300

    feet: finish set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 
-export default function Home() {
+export default async function Home() {
+
+  const games = await prisma.game.findMany()
+  console.log(games);
+
   return (
     <div>
       <Button size={'lg'} variant={"outline"}> GG</Button>
+
+      {games.map((game) => (
+        <p key={game.id}>{game.name}</p>
+      ))}
     </div>
   );
 }
